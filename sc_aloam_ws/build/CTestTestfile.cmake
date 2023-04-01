4a53e1157de4bf8b82935df82eed92986a903be5
CharlieZhurp <128444949+CharlieZhurp@users.noreply.github.com>
2023-04-01 17:15:32 +0800

    Delete sc_aloam_ws directory

--- sc_aloam_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zhu/ME5413_Final_Project/sc_aloam_ws/src
-# Build directory: /home/zhu/ME5413_Final_Project/sc_aloam_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("SC-A-LOAM")
--- sc_aloam_ws/build/SC-A-LOAM/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zhu/ME5413_Final_Project/sc_aloam_ws/src/SC-A-LOAM
-# Build directory: /home/zhu/ME5413_Final_Project/sc_aloam_ws/build/SC-A-LOAM
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
